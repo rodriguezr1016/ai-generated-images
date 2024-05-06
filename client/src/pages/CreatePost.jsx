ed56f0a7acf0d163d6a3a8985c4e6d6d132a28a1
Rene Rodriguez <134916213+rodriguezr1016@users.noreply.github.com>
2024-05-06 20:51:57 +0000

    Home page and a little bit of the CreatePost page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
-
+import {React, useState} from 'react'
+import {useNavigate} from 'react-router-dom'
+import {preview} from '../assets'
+import {getRandomPrompt} from '../utils'
+import {FormField, Loader} from '../components'
 const CreatePost = () => {
+    const navigate = useNavigate();
+    const [form, setForm] = useState({
+        name: '',
+        prompt: '',
+        photo: '',
+    });
+    const [generatingImg, setGeneratingImg] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const handleSubmit = () => {
+
+    };
   return (
-    <div>CreatePost</div>
+    <section className="max-w-7xl mx-auto">
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">Create stunning images using DALL-E AI and share them with the world</p>
+        <form onSubmit={handleSubmit} className="mt-16 max-w-3xl">
+            <div className="flex flex-col gap-5">
+                <FormField 
+                labelName= "Your name"
+                type= 'text'
+                name= 'name'
+                placeholder= 'John Doe'
+                />
+            </div>
+        </form>
+
+    </section>
   )
 }
 
