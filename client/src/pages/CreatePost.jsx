5ce26ff09ebaf8fe3edc76c411acf8a3adf8d12a
Rene Rodriguez <134916213+rodriguezr1016@users.noreply.github.com>
2024-05-08 02:17:02 +0000

    Create page completed

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -12,8 +12,20 @@ const CreatePost = () => {
     });
     const [generatingImg, setGeneratingImg] = useState(false);
     const [loading, setLoading] = useState(false);
+    const [generated, setgenerated] = useState(true)
     const handleSubmit = () => {
 
+    };
+    const generateImage = () => {
+
+    };
+    const handleChange = (e) => {
+        setForm({...form, [e.target.name] : e.target.value})
+
+    };
+    const handleSurpriseMe = () => {
+        const randomPrompt = getRandomPrompt(form.prompt)
+        setForm({...form, prompt: randomPrompt})
     };
   return (
     <section className="max-w-7xl mx-auto">
@@ -26,8 +38,40 @@ const CreatePost = () => {
                 type= 'text'
                 name= 'name'
                 placeholder= 'John Doe'
+                value = {form.name}
+                handleChange = {handleChange}
                 />
+                <FormField 
+                labelName= "Prompt"
+                type= 'text'
+                name= 'prompt'
+                placeholder= 'The long-lost Star Wars 1990 Japanese Anime'
+                value = {form.prompt}
+                handleChange = {handleChange}
+                isSurpriseMe
+                handleSurpriseMe={handleSurpriseMe}
+                />
+                <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+                    {form.photo ? (<img src={form.photo} alt={form.prompt} className='w-full h-full object-contain'/>) : (<img src={preview} alt='preview' className='w-9/12 h-9/12 object-contain opacity-40'/>)}
+                    {generatingImg && ( 
+                    <div className='absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg'>
+                        <Loader />
+                    </div>)}
+                </div>
             </div>
+            <div className="mt-5 flex gap-5">
+                <button type='button' className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"onClick={generateImage}>
+                    {generatingImg ? 'Generating Image...': 'Generate Image'}
+                </button>
+            </div>
+
+            {generated ? (<div className="mt-10">
+                <p className='mt-2 text-[#666e75] text-[14px]'>Once you've created your image share it with the community</p>
+                <button className="mt-3 text-white bg-[#6369ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+                    {loading ? 'Sharing' : 'Share'}
+                </button>
+            </div>) : (<></>)}
+
         </form>
 
     </section>
