a442b45f8eb8d50475d18ef1ad30f2a93c14ea46
Rene Rodriguez <134916213+rodriguezr1016@users.noreply.github.com>
2024-06-10 23:32:12 +0000

    make it better

--- server/index.js
+++ server/index.js
@@ -73,10 +73,11 @@ app.post('/api/login',async(req, res) =>{
       res.status(500).send(error.message);
   }
 });
+const PORT = process.env.PORT || 8080;
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () => console.log('Server started on port 8080'));
+    app.listen(PORT, () => console.log('Server started on port 8080'));
   } catch (error) {
     console.log(error);
   }
