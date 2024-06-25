fb720bd0e4d4233bfffbfb88357777dc78f9fe01
Rene Rodriguez <134916213+rodriguezr1016@users.noreply.github.com>
2024-06-25 05:14:33 +0000

    I updated the openai package to the latest model

--- server/routes/dalleRoute.js
+++ server/routes/dalleRoute.js
@@ -1,30 +1,28 @@
 import express, { response } from 'express';
 import * as dotenv from 'dotenv';
-import {Configuration, OpenAIApi} from 'openai';
+import OpenAI from 'openai';
 
 dotenv.config();
 
 const router = express.Router();
 
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-})
-
-const openai = new OpenAIApi(configuration);
-
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+  });
 router.route('/').get((req, res) => {
     res.send("Hello from DALLE");
 })
 router.route('/').post(async (req, res) => {
     try {
         const {prompt} = req.body
-        const aiResponse = await openai.createImage({
+        const aiResponse = await openai.images.generate({
             prompt,
+            model: "dall-e-3",
             n: 1,
             size: '1024x1024',
             response_format: 'b64_json'
         });
-        const image = aiResponse.data.data[0].b64_json
+        const image = aiResponse.data[0].b64_json
         res.status(200).json({photo: image})
     } catch (error) {
         console.log(error);
